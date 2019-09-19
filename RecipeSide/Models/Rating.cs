4836bd21bbab75a90fb2f6dd77d7ec982d1899a0
Jimmy Martin Henriksen <jimm1034@edu.ucl.dk>
2019-09-19 10:07:10 +0200

    Added range to character in rating

--- RecipeSide/Models/Rating.cs
+++ RecipeSide/Models/Rating.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace RecipeSide.Models
 {
     public class Rating
     {
+        [Range(1, 5)]
         public int Character { get; set; }
     }
 }
